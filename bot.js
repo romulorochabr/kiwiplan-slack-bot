a3267a35201daf48a4c9f81a52bde113dbf53516
Haoyang Feng <haoyangnz@gmail.com>
2016-03-18 17:27:41 +1300

    This might stop the bot from crashing when Melody's card doesn't have a SCM number

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ controller.setupWebserver(process.env.PORT || 3000);
 controller.spawn({token : 'xoxb-21068424885-QfOIy7hxdbX3fakXluS5ka2e' }).startRTM();
 
 controller.on('bot_message', function(bot, message) {
-	if (message.channel == 'C0JAB2CAD' && message.bot_id == 'B0HSGEXF1' && message.attachments && message.attachments[0] && message.attachments[0].text && message.attachments[0].text.indexOf('Melody') > -1) {
+	if (message.channel == 'C0JAB2CAD' && message.bot_id == 'B0HSGEXF1' && message.attachments && message.attachments[0] && message.attachments[0].text && message.attachments[0].text.indexOf('Melody') > -1 && message.attachments[0].text.match(/\d{6}/g)) {
 		bot.say({ channel: 'C0M20LYJF', text: 'https://kall.kiwiplan.co.nz/scm/softwareChangeViewer.do?id=' + message.attachments[0].text.match(/\d{6}/g)[0]});
 	}
 });
