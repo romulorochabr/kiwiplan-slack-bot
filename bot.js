01aa96936d99742a1482c4224f672dd1c14a5f68
Haoyang Feng <haoyangnz@gmail.com>
2016-04-29 10:33:42 +1200

    Fixed issue where json cannot contain special characters

--- bot.js
+++ bot.js
@@ -9,10 +9,10 @@ bot.startRTM();
 
 
 var channels = {
-	test-jack: 'C14N0EPGC',
-	_dev: 'C0L4PU8S1',
-	_qa-preview: 'C0M20LYJF',
-	_planning: 'C0JAB2CAD'
+	testjack: 'C14N0EPGC',
+	dev: 'C0L4PU8S1',
+	qapreview: 'C0M20LYJF',
+	planning: 'C0JAB2CAD'
 };
 
 var bots = {
@@ -21,8 +21,8 @@ var bots = {
 
 
 controller.on('bot_message', function(bot, message) {
-	if (message.channel == channels._planning && message.bot_id == bots.trello && message.attachments && message.attachments[0] && message.attachments[0].text && message.attachments[0].text.indexOf('Melody') > -1 && message.attachments[0].text.match(/\d{6}/g)) {
-		bot.say({ channel: channels._qa-preview, text: 'https://kall.kiwiplan.co.nz/scm/softwareChangeViewer.do?id=' + message.attachments[0].text.match(/\d{6}/g)[0]});
+	if (message.channel == channels.planning && message.bot_id == bots.trello && message.attachments && message.attachments[0] && message.attachments[0].text && message.attachments[0].text.indexOf('Melody') > -1 && message.attachments[0].text.match(/\d{6}/g)) {
+		bot.say({ channel: channels.qapreview, text: 'https://kall.kiwiplan.co.nz/scm/softwareChangeViewer.do?id=' + message.attachments[0].text.match(/\d{6}/g)[0]});
 	}
 });
 
@@ -31,7 +31,5 @@ controller.hears('Hi', ['direct_message'], function(bot, message) {
 });
 
 setInterval(function() {
-	console.log("AAAA");
-	console.log(bot.say);
-	bot.say({ channel: channels.test-jack, text: 'Testing jack. Please ignore.'});
-}, 10000);
+	bot.say({ channel: channels.testjack, text: 'Testing jack. Please ignore.'});
+}, 3000);
