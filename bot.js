9734a3ae192e2953f86864600f48d48bbf7f4976
Haoyang Feng <haoyangnz@gmail.com>
2016-05-27 14:37:25 +1200

    Automatically assign devs onto top dev cards

--- bot.js
+++ bot.js
@@ -6,6 +6,11 @@ var Trello = require('node-trello');
 var string = function(input) {
 	return JSON.stringify(input, null, 2);
 }
+var l = function(title, input) {
+	console.log(title);
+	console.log(string(input));
+	console.log();
+}
 
 // Start Slack Connection
 var controller = Botkit.slackbot({ debug : false });
@@ -19,20 +24,26 @@ var trello = new Trello('3c3032368c3c88ac3ba8799f3e37d935', 'ee99dec582dbbbacf02
 // User IDs
 var users = {
 	haoyang: {
+		name: 'haoyang',
 		slack: 'U0HMLSLKY',
 		trello: '53ed667b3f5d4e4c4e1c5902'
 	},
 	ushal: {
+		name: 'ushal',
 		slack: 'U0HMMNE9W',
 		trello: '563fc2beb2e713d534da52ce'
 	},
 	melody: {
+		name: 'melody',
 		slack: 'U0J4CGQKW',
 		trello: '5578c1e12f582a666e7bca4a'
-	},
+	}
 };
-var trellouser = function(message) {
-	return _.find(users, { slack: message.user }).trello;
+var t2n = function(t) {
+	return _.find(users, { trello: t }).name;
+};
+var n2t = function(n) {
+	return _.find(users, { name: n }).trello;
 };
 
 // Slack IDs
@@ -62,7 +73,7 @@ controller.hears('trello', ['direct_message'], function(bot, message) {
 	trello.get('/1/boards/' + boards.ult + '/lists', { filter: 'open', fields: 'name' }, function(err, lists) {
 		var devlistid = _.find(lists, function(list) { return list.name.indexOf('Dev Sprint') > -1; }).id;
 		trello.get('/1/lists/' + devlistid + '/cards', { filter: 'open', fields: 'name,idMembers' }, function(err, cards) {
-			var mycards = _.filter(cards, function(card) { return _.includes(card.idMembers, trellouser(message)) });
+			var mycards = _.filter(cards, function(card) { return _.includes(card.idMembers, n2t(message.user)) });
 			bot.reply(message, string(_.map(mycards, 'name')));
 		});
 	});
@@ -91,9 +102,50 @@ controller.on('bot_message', function(bot, message) {
 	}
 });
 
+// Trello Utility
+var tlists = function(cb) {
+	trello.get('/1/boards/' + boards.ult + '/lists', { filter: 'open', fields: 'name' }, function(err, lists) {
+		cb(lists);
+	});
+};
+var tcards = function(listnameprefix, cb) {
+	tlists(function(lists) {
+		var devlistid = _.find(lists, function(list) { return list.name.indexOf(listnameprefix) > -1; }).id;
+		trello.get('/1/lists/' + devlistid + '/cards', { filter: 'open', fields: 'name,idMembers' }, function(err, cards) {
+			cb(cards);
+		});
+	});
+};
+
 // SCM Integration
+var scmusers = ['haoyang', 'ushal'];
 setInterval(function() {
-	
+	tcards('Test', function(cards) {
+		// XXX This implementation is a bit unpleasant
+		var userstoassign = _.clone(scmusers);
+		var priorityassignee = _.clone(scmusers);
+		for (var i = 0; i < userstoassign.length; i++) {
+			var card = cards[i];
+			for (var j = 0; j < card.idMembers.length; j++) {
+				_.pull(priorityassignee, t2n(card.idMembers[j]));
+			};
+		};
+		_.each(cards, function(card) {
+			if (_.isEmpty(card.idMembers)) {
+				var usertoassign = _.sample(_.isEmpty(priorityassignee) ? userstoassign : priorityassignee);
+				_.pull(priorityassignee, usertoassign);
+				_.pull(userstoassign, usertoassign);
+				trello.post('/1/cards/' + card.id + '/idMembers', { value: n2t(usertoassign) }, function(err) {});
+			}
+			else {
+				for (var i = 0; i < card.idMembers.length; i++) {
+					_.pull(priorityassignee, t2n(card.idMembers[i]));
+					_.pull(userstoassign, t2n(card.idMembers[i]));
+				};
+			}
+			return !_.isEmpty(userstoassign);
+		});
+	});
 }, 5000);
 
 // VM Warnings
