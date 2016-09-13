f23c6de83ebf54f4880bed1e938aad5e36610c82
Ushal Naidoo <ushal7naidoo@gmail.com>
2016-09-13 14:00:57 +1200

    Adjusted the message on test command

--- bot.js
+++ bot.js
@@ -493,7 +493,7 @@ controller.on('ambient', function(bot, message) {
 	else if (message.text.indexOf('test') == 0) {
 		channelname(message.channel, function(name) {
 			tfcode(name, function(card) {
-				bot.reply(message, '<@melo>: Please test. (accept/reject)');
+				bot.reply(message, '<@melo>: Please test. (Please type teststart when you begin and teststop when you end. Then type accept/reject when completed)');
 				tassign(card, 'melody');
 			});
 		});
